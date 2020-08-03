7b6f85bb4024599d0a698b941ddff9217e5196b0
Jason T <jason@jasont.dev>
2020-08-03 13:57:22 +1000

    Preparing to change remote

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>day-off dashboard</h1>
+      <div v-if="remainingDaysOff.length > 0">
+        <h3>next day-off</h3>
+          {{ remainingDaysOff[0].toDateString() }} in {{ numDaysToNextDayOff }} days.
+        <div v-if="remainingDaysOff.length > 1"><h3>remaining days-off</h3>
+          <table class="table table-striped">
+            <tbody>
+            <tr v-for="dayOff in remainingDaysOff.slice(1)" :key="dayOff.toString()">
+              <td>{{ dayOff.toDateString() }}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div v-else>
+        <p>Sorry, you don't have any days-off remaining.</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      daysOffFromApi: [],
+    }
+  },
+  mounted() {
+    this.fetchDaysOffFromApi();
+  },
+  computed: {
+    allDaysOff() {
+      return this.daysOffFromApi.map(dayOff => new Date(Date.parse(dayOff.date)));
+    },
+    apiUri() {
+      return process.env.VUE_APP_API_URI;
+    },
+    remainingDaysOff() {
+      return this.allDaysOff.filter(dayOff => dayOff > Date.now())
+    },
+    numDaysToNextDayOff() {
+      return Math.floor((this.remainingDaysOff[0] - Date.now()) / 86400000);
+    }
+  },
+  methods: {
+    fetchDaysOffFromApi() {
+      fetch(this.apiUri)
+          .then(response => response.json())
+          .then(response => this.daysOffFromApi = response.dates)
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
